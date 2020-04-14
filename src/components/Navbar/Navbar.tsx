69824bc7f7b0db6f25b07bdd6467eefef6b40e95
Sean M <sean@arrowburn.co.nz>
2020-04-14 22:14:22 +1200

    chore: update navbar img links

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,10 +5,12 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="left-container">
-        <img height="45" width="150" alt="Built With Taquito logo" src="taquito.png" />
+        <a href="https://github.com/ecadlabs/taquito">
+          <img height="45" width="150" alt="Taquito logo" src="taquito.png" />
+        </a>
       </div>
       <div className="right-container">
-        <a href="https://github.com/ecadlabs/taquito">
+        <a href="https://github.com/ecadlabs/bjm">
           <img alt="Github logo" height="55" width="100" src="github.jpg" />
         </a>
       </div>
