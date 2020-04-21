da62be24b50d79f69f9d79d2e9019ce33fa7e4c5
Sean Magin <sean@arrowburn.co.nz>
2020-04-21 23:41:55 +1200

    chore: update for mobile rendering

--- src/components/WizardControls/index.tsx
+++ src/components/WizardControls/index.tsx
@@ -22,27 +22,29 @@ const Dots = (props: WizardControlsProps): ReactElement => {
   };
 
   return (
-    <div className="dots-container">
+    <div className="wizard-container">
       <div className="wizard-steps">
         <div onClick={handleStepClick}>
-          <span className={`${code.length > 0 ? "dot completed" : "dot"} ${currentStep === 1 ? "active" : ""}`}>1</span>
+          <span className={`${code.length > 0 ? "step completed" : "step"} ${currentStep === 1 ? "active" : ""}`}>
+            1
+          </span>
           <br />
           Fetch Contract
         </div>
         <div onClick={handleStepClick}>
-          <span className={` ${code.length > 0 ? "dot completed" : "dot"} ${currentStep === 2 ? "active" : ""}`}>
+          <span className={` ${code.length > 0 ? "step completed" : "step"} ${currentStep === 2 ? "active" : ""}`}>
             2
           </span>
           <br />
           Review Contract
         </div>
         <div onClick={handleStepClick}>
-          <span className={`${signer ? "dot completed" : "dot"} ${currentStep === 3 ? "active" : ""}`}>3</span>
+          <span className={`${signer ? "step completed" : "step"} ${currentStep === 3 ? "active" : ""}`}>3</span>
           <br />
           Deploy New Contract
         </div>
         <div onClick={handleStepClick}>
-          <span className={`${txnAddress ? "dot completed" : "dot"} ${currentStep === 4 ? "active" : ""}`}>4</span>
+          <span className={`${txnAddress ? "step completed" : "step"} ${currentStep === 4 ? "active" : ""}`}>4</span>
           <br />
           View New Contract
         </div>
