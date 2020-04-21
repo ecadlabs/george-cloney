458d3f8a3056a869ab23a9463e2538c133074bcd
Sean Magin <sean@arrowburn.co.nz>
2020-04-21 20:14:28 +1200

    chore: update step system UI

--- src/components/WizardControls/index.tsx
+++ src/components/WizardControls/index.tsx
@@ -10,8 +10,23 @@ const Dots = (props: WizardControlsProps): ReactElement => {
       <span onClick={() => setCurrentStep(1)} className={`${currentStep === 1 ? "hide-step" : "previous-step"}`}>
         Prev Step
       </span>
-      <span className={`${code.length > 0 ? "dot completed" : "dot"} ${currentStep === 1 ? "active" : ""}`}>1</span>
-      <span className={` ${signer ? "dot completed" : "dot"} ${currentStep === 2 ? "active" : ""}`}>2</span>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div style={{ display: "inline !important", marginRight: "20px", marginLeft: "20px" }}>
+          <span className={`${code.length > 0 ? "dot completed" : "dot"} ${currentStep === 1 ? "active" : ""}`}>1</span>
+          <br />
+          Fetch Contract
+        </div>
+        <div style={{ display: "inline !important", marginRight: "20px", marginLeft: "20px" }}>
+          <span className={` ${signer ? "dot completed" : "dot"} ${currentStep === 2 ? "active" : ""}`}>2</span>
+          <br />
+          Review Contract
+        </div>
+        <div style={{ display: "inline !important", marginRight: "20px", marginLeft: "20px" }}>
+          <span className={`${code.length > 0 ? "dot completed" : "dot"} ${currentStep === 3 ? "active" : ""}`}>3</span>
+          <br />
+          Sign/Deploy Contract
+        </div>
+      </div>
       <button
         onClick={() => setCurrentStep(2)}
         disabled={code.length > 0 ? false : true}
