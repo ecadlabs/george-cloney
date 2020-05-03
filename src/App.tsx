26466bfbd79d93f16ea475ac42f3be1357521900
Sean Magin <sean@arrowburn.co.nz>
2020-05-03 18:04:30 +1200

    fix: update subheader

--- src/App.tsx
+++ src/App.tsx
@@ -222,8 +222,8 @@ const App: React.FC = (): ReactElement => {
         <div className="title-group">
           <img alt="George Cloney signature in cursive" src={georgeCloneyTitleImg} />
           <h4>
-            George Cloney, being the gentleman he is, will take any Tezos Smart Contract and clone it for you. Great for
-            testing and exploring.
+            George Cloney will find any Tezos Smart Contract and clone it for you. <br /> Great for testing and
+            exploring.
           </h4>
         </div>
         <SnackbarGroup
