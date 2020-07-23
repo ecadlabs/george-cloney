29ab512eb2d7797a138c88ca75fc146c5f330c4a
claudebarde <claudebarde@users.noreply.github.com>
2020-07-23 05:54:02 +0000

    deploy: 31577f2ffd014948a03207897eb0aef5e8d43c00

---
+++ precache-manifest.dcd0cba70482d487eea67e712e95e3d2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7010a6e32fd196344527ffaa8c5fa7ea",
+    "url": "/george-cloney/index.html"
+  },
+  {
+    "revision": "1b709078b8619fa42d34",
+    "url": "/george-cloney/static/css/main.b79ee4c7.chunk.css"
+  },
+  {
+    "revision": "cc55a4df5c96635fbaf9",
+    "url": "/george-cloney/static/js/2.212c759c.chunk.js"
+  },
+  {
+    "revision": "ef6af18e435709d44678929b7cc798fc",
+    "url": "/george-cloney/static/js/2.212c759c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b709078b8619fa42d34",
+    "url": "/george-cloney/static/js/main.7dae52c3.chunk.js"
+  },
+  {
+    "revision": "8deedc3e89417e283d81",
+    "url": "/george-cloney/static/js/runtime-main.02366c6e.js"
+  },
+  {
+    "revision": "b00f5f7479196eb2360730444afd97f6",
+    "url": "/george-cloney/static/media/george-cloney-title.b00f5f74.png"
+  },
+  {
+    "revision": "399f1c6d1de88c9f8db54a90ff1199b6",
+    "url": "/george-cloney/static/media/george-cloney.399f1c6d.png"
+  },
+  {
+    "revision": "24b62548b48af3e019a3c82df54758a8",
+    "url": "/george-cloney/static/media/github.24b62548.jpg"
+  }
+]);
