d56961cd883686e82d21da7bbf3e087b5e905b5a
Sean Magin <sean@arrowburn.co.nz>
2020-08-30 09:57:23 +1200

    fix: remove scaffolidng for bugsnag

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import Bugsnag from "@bugsnag/js";
-// import BugsnagPluginReact from "@bugsnag/plugin-react";
 
-// Bugsnag.start({
-//   apiKey: process.env.BUGSNAG,
-//   plugins: [new BugsnagPluginReact()],
-// });
-
-const ErrorBoundary = (Bugsnag.getPlugin("react") as any).createErrorBoundary(React);
-
-ReactDOM.render(
-  <ErrorBoundary>
-    <App />
-  </ErrorBoundary>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
