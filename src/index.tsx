775a19a4190e1d8ff4be029fc49eebb7317fcd4a
Sean Magin <sean@arrowburn.co.nz>
2020-09-01 18:13:16 +1200

    add bugsnag

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import Bugsnag from "@bugsnag/js";
+import BugsnagPluginReact from "@bugsnag/plugin-react";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+Bugsnag.start({
+  apiKey: process.env.REACT_APP_BUGSNAG as string,
+  plugins: [new BugsnagPluginReact()],
+});
+
+const ErrorBoundary = (Bugsnag.getPlugin("react") as any).createErrorBoundary(React);
+
+ReactDOM.render(
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>,
+  document.getElementById("root")
+);
