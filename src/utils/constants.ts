faa377b0734288742ff138ff3e763d03e41584a7
Sean Magin <sean@arrowburn.co.nz>
2020-11-02 16:26:38 +1300

    update for testnet reusability

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,5 @@
 export const TEST_NETWORK = "delphinet";
+export const TEST_NETWORKS = ["carthagenet", "delphinet"];
 export const MAIN_NETWORK = "mainnet";
 export enum NetworkType {
   MAINNET = "mainnet",
